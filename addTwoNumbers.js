0cb89ba30cc4234a87dab7f7774fa46b3b3ee717
Anthony Ruffin <anthonyruffin2010@gmail.com>
2017-10-28 21:37:34 -0400

    edited function

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -3,7 +3,8 @@ function addTwoNumbers(){
     var numberOne = parseInt(document.getElementById("number-box-one").value);
     var numberTwo = parseInt(document.getElementById("number-box-two").value);
     var total = document.getElementById("sum-of-numbers");
-
+    
+    //Result
     total.value = numberOne + numberTwo;
 }
 
